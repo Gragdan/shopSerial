33c87e1d9b29cdef1032fef863419c5780caf3b4
Andrei Grazhdan <myway@yandex-team.ru>
2022-10-06 08:45:08 +0700

    файлы

---
+++ src/Basket.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Basket {
+    private  String[] products;
+    private double[] prices;
+    private  int[] selected;
+
+    public Basket(String[] products, double[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.selected = new int[prices.length];
+    }
+    private Basket(String[] products, double[] prices, int[] selected) {
+        this.products = products;
+        this.prices = prices;
+        this.selected = selected;
+    }
+
+    public void saveTxt(File txtFile) throws FileNotFoundException {
+        // loadFromTxtFile(file);
+        try (PrintWriter writer = new PrintWriter(txtFile)) {
+            for (String product : products) {
+                writer.print(product + " ");
+            }
+            writer.println();
+            for (double price : prices) {
+                writer.print(price + " ");
+            }
+            writer.println();
+            for (int sel : selected) {
+                writer.print(sel + " ");
+            }
+
+        }
+    }
+    public static Basket  loadFromTxtFile( File txtFile) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream(txtFile))) {
+            String[] products = scanner.nextLine().trim().split(" ");
+            double[] prices = Arrays.stream(scanner.nextLine().trim().split(" "))
+                    .mapToDouble(Double::parseDouble)
+                    .toArray();
+            int[] selected = Arrays.stream(scanner.nextLine().trim().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            return new Basket(products, prices, selected);
+        }
+    }
+
+
+    public int[] addToCart(int productNum, int amount) {
+        selected[productNum - 1] = selected[productNum - 1] + amount;
+        return selected;
+    }
+
+    public void printCart() {
+        System.out.println("Goods in yuor cart: ");
+        for (int i = 0; i < products.length; i++) {
+            if (selected[i] != 0) {
+                System.out.println(products[i] + ", prise: " + prices[i] + "/pie, quantity: " + selected[i] + " sum  " + prices[i] * selected[i]);
+            }
+        }
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        String[] products = {"cheese", "bread"};
+        double[] prices = {12.5, 4};
+        File txtFile = new File("basket.txt");
+        //  File binFile = new File("basket.bin");
+        Basket myBasket = null;
+        if (txtFile.exists()) {
+            //   myBasket = Basket.loadFromBinFile(binFile);
+            myBasket = Basket.loadFromTxtFile(txtFile);
+        } else {
+            //   binFile.createNewFile();
+            txtFile.createNewFile();
+            myBasket = new Basket(products, prices);
+        }
+        myBasket.printCart();
+        System.out.println("Products for sale:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(i + 1 + " " + products[i] + "  " + prices[i] + " $/pie");
+        }
+
+        while (true) {
+            System.out.println("Choose number and quant of goods or inter `end`");
+
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                myBasket.printCart();
+                break;
+            }
+            int currentProduct;
+            int currentQuan;
+            String parts[] = input.split(" ");
+            if (parts.length == 2) {
+                try {
+                    currentProduct = Integer.parseInt(parts[0]);
+                    currentQuan = Integer.parseInt(parts[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Одно из введенных значений, либо оба - не являются числом!");
+                    continue;
+                }
+
+                if (currentProduct < 1 || currentProduct > products.length) {
+                    System.out.println("Товара под номером " + currentProduct + " не существует!");
+                    continue;
+                }
+
+                if (Integer.parseInt(parts[1]) < 0) {
+                    System.out.println("Нельзя выбрать такое количество товара! ");
+                    continue;
+                }
+                myBasket.addToCart(currentProduct, currentQuan);
+                myBasket.saveTxt(txtFile);
+            } else {
+                System.out.println("Введено неверное количество чисел! ");
+                continue;
+            }
+
+        }
+
+    }
+}
+
